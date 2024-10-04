4182a1f26eae1046b713a9b965cb3855d3b081ef
Nomad37301 <agungpurnama100@gmail.com>
2024-10-04 22:38:26 +0800

    01

---
+++ .vscode/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout<<"masukan tahun lahir :"; cin>>a;
+    cout<<"masukan tahun sekarang :"; cin>>b;
+
+    cout<<b<< " - " <<a<< " = " << b-a << " tahun";
+    return 0;
+}
